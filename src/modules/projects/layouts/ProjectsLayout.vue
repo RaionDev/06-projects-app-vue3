2ba8141a0bc37f708f07e3116bbd48e439282e6c
leokeeper <leonardop.keeper@gmail.com>
2024-12-04 20:33:55 -0500

    changes

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">
+    <main>
+      <top-menu />
+      <router-view />
+    </main>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+
+
+</script>
+
+<style scoped></style>
